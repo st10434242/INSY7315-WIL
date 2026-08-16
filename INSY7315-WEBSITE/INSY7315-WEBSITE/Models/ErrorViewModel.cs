f6ed61b83dfe8db5c7e9cb9975013a428fdefadb
Divan Fourie <161314327+st10434242@users.noreply.github.com>
2026-08-16 20:16:55 +0200

    Added website project and moved app into sub folder

---
+++ INSY7315-WEBSITE/INSY7315-WEBSITE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace INSY7315_WEBSITE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
